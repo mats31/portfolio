8383a200d94e804043b0452eaf4ee2f3d3032cbe
Mathis Biabiany <mathis.biabiany@touaregs.com>
2016-01-19 15:42:14 +0100

    Add shader to project

--- app/shaders/projectFragment.glsl
+++ app/shaders/projectFragment.glsl
@@ -1,11 +1,11 @@
-uniform vec3 color;
 uniform sampler2D texture;
+
 
 varying vec3 vColor;
 
 void main() {
 
-  gl_FragColor = vec4( color * vColor, 1.0 );
-  gl_FragColor = gl_FragColor;
+  gl_FragColor = vec4( vColor, 1.0 );
+  // gl_FragColor = gl_FragColor;
 
 }
--- app/shaders/projectVertex.glsl
+++ app/shaders/projectVertex.glsl
@@ -1,5 +1,5 @@
 attribute float size;
-attribute vec3 customColor;
+attribute vec3 color;
 attribute float startPosition;
 
 
@@ -7,7 +7,7 @@ varying vec3 vColor;
 
 void main() {
 
-  vColor = customColor;
+  vColor = color;
 
   vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
   gl_Position = projectionMatrix * mvPosition;
